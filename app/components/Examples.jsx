02e4beac667043942f15152bb68242ce1aaa391a
gerdru <g_druschke@mail.com>
2016-09-04 16:39:12 -0400

    style example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 var Examples = (props) => {
   return (
     <div>
-    <h3>Examples</h3>
-    <p>Welcome to the Examples page!</p>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out:</p>
+    <ol>
+      <li><Link to='/?location=Asuncion'>Asuncion, PY</Link></li>
+      <li><Link to='/?location=Berlin, GER'>Berlin, GER</Link></li>
+    </ol>
     </div>
   )
 };
